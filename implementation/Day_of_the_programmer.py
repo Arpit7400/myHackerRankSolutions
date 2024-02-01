b473f1467435c38dae95269aedf2a4f65aa30e2c
Arpit Sahu <137257385+Arpit7400@users.noreply.github.com>
2024-02-01 22:03:27 +0530

    Add files via upload

---
+++ implementation/Day_of_the_programmer.py
@@ -0,0 +1,15 @@
+# import time
+# # begin = time.time()
+# # lst = []
+# # for i in range (0,9999):
+# #     for j in range(i):
+# #         lst.append(i+j)
+# # end = time.time()
+# # print("time taken",end-begin)
+
+
+
+
+
+
+
